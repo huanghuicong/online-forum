d6c063bd596db29bfcd7a49d4e641ff3fee9aa80
huanghuicong <2278614449@qq.com>
2020-10-27 00:42:41 +0800

    redis测试

---
+++ online-forum-api/src/main/java/com/web/onlineforumapi/demo/TestSwitch.java
@@ -0,0 +1,18 @@
+package com.web.onlineforumapi.demo;
+
+public class TestSwitch {
+    public static void main(String[] args) {
+        int i = 0;
+        switch(i){
+            case 0 :
+                System.out.println("000");
+            case 1 :
+                System.out.println("111");
+            case 2 :
+                System.out.println("222");
+                break;
+            default :
+                System.out.println("333");
+        }
+    }
+}
