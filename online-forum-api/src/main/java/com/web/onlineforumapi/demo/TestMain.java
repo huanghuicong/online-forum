9480d2120377e9a77ecfc8ca01bf329e554e412c
huanghuicong <2278614449@qq.com>
2020-11-23 18:49:47 +0800

    redis测试

--- online-forum-api/src/main/java/com/web/onlineforumapi/demo/TestMain.java
+++ online-forum-api/src/main/java/com/web/onlineforumapi/demo/TestMain.java
@@ -1,17 +1,30 @@
 package com.web.onlineforumapi.demo;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class TestMain {
     public static void main(String[] args) {
-        ReentrantLock reentrantLock = new ReentrantLock();
+        /*ReentrantLock reentrantLock = new ReentrantLock();
         ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock(false);
         reentrantLock.lock();
         reentrantLock.unlock();
         Lock lock = new ReentrantLock(true);
         lock.tryLock();
-        lock.unlock();
+        lock.unlock();*/
+        int sum = 0;
+        int k = 80;
+        for (int i = 0; i < 100; i++) {
+            sum = sum + k;
+            k = k + 5;
+            if (k == 170) {
+                break;
+            }
+        }
+        System.out.println(sum);
+        Map map = new ConcurrentHashMap();
     }
 }
