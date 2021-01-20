727b4fb10a9b49d33fb22ebe798d500892b39e50
huanghuicong <2278614449@qq.com>
2021-01-20 19:15:42 +0800

    nacos测试

--- online-forum-service/src/main/java/com/web/onlineforumservice/OnlineForumServiceApplication.java
+++ online-forum-service/src/main/java/com/web/onlineforumservice/OnlineForumServiceApplication.java
@@ -2,7 +2,9 @@ package com.web.onlineforumservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class OnlineForumServiceApplication {
 
